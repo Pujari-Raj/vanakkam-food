eb14ce8134056cd0d56fa672c722aaca554bc0b9
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-10-23 12:29:22 +0530

    slight changes in components

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,15 +4,16 @@ import MenuItemBox from "./MenuItemBox";
 
 const Accordion = ({ data, isVeg, restInfo }) => {
   const [isVisible, setisVisible] = useState(true);
+  //
   const calcLength = data?.itemCards
     ? data?.itemCards?.filter(
-        (data) =>
-          isVeg || data?.card?.info?.itemAttribute?.vegClassifier === "VEG"
-      )?.length
+      (data) =>
+        isVeg || data?.card?.info?.itemAttribute?.vegClassifier === "VEG"
+    )?.length
     : data?.categories?.length;
 
   return (
-    <div  className="menuAccordion scroll-mt-20 border-b-[16px] border-b-[#f1f1f6] px-0 py-4" id={data?.title}>
+    <div className="menuAccordion scroll-mt-20 border-b-[16px] border-b-[#f1f1f6] px-0 py-4" id={data?.title}>
       <div
         className="flex w-full cursor-pointer items-center justify-between text-base font-bold leading-[1.2] text-[#3e4152] transition-all duration-[0.2s]"
         onClick={() => {
@@ -28,28 +29,29 @@ const Accordion = ({ data, isVeg, restInfo }) => {
         <div className="itemBody">
           {data?.itemCards
             ? data?.itemCards
-                ?.filter(
-                  (data) =>
-                    isVeg ||
-                    data?.card?.info?.itemAttribute?.vegClassifier === "VEG",
-                )
-                .map((data, idx) => {
-                  return (
-                    <MenuItemBox data={data?.card} key={idx} restInfo={restInfo} />
-                  );
-                })
-            : data?.categories?.map((data, idx) => {
+              ?.filter(
+                (data) =>
+                  isVeg ||
+                  data?.card?.info?.itemAttribute?.vegClassifier === "VEG",
+              )
+              .map((data, idx) => {
                 return (
-                  <Accordion
-                    data={data}
-                    key={idx}
-                    isVeg={isVeg}
-                    restInfo={restInfo}
-                  />
+                  <MenuItemBox data={data?.card} key={idx} restInfo={restInfo} />
                 );
-              })}
+              })
+            : data?.categories?.map((data, idx) => {
+              return (
+                <Accordion
+                  data={data}
+                  key={idx}
+                  isVeg={isVeg}
+                  restInfo={restInfo}
+                />
+              );
+            })}
         </div>
-      )}
+      )
+      }
     </div>
   );
 };
