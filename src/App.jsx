c266bcbf4e943cfcccc19dfed2d03fc90c113ebe
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-10-29 23:51:35 +0530

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes,  } from "react-router-dom";
 import Body from "./components/Body";
 import LandingPage from "./components/LandingPage";
 import HomePage from "./pages/HomePage";
