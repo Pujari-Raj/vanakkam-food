da5334e91f617ccdb19715198ed35d3a6ee17252
Pujari Basvaraj <basvarajpujari607@gmail.com>
2024-01-09 14:05:24 +0530

    solved cors error and added shimmer component for homepage restaurants

---
+++ src/components/MoreRestaurantsShimmer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const MoreRestaurantsShimmer = () => {
+  return (
+    <>
+      <div className="container-card_shimmer1 shine min-w-[150px] h-[280px] rounded-2xl ">
+        <div className="rest_img shine  h-[55%]"></div>
+
+        <div className="rest_info_card h-1/4">
+          <p className="name_of_hotel_shimmer wrapper_shimmer"></p>
+          <div className=" wrapper_shimmer content-line"></div>
+          <div className=" wrapper_shimmer content-line"></div>
+        </div>
+      </div>
+      <div className="container-card_shimmer1 shine min-w-[150px] h-[280px] rounded-2xl ">
+        <div className="rest_img shine  h-[55%]"></div>
+
+        <div className="rest_info_card h-1/4">
+          <p className="name_of_hotel_shimmer wrapper_shimmer"></p>
+          <div className=" wrapper_shimmer content-line"></div>
+          <div className=" wrapper_shimmer content-line"></div>
+        </div>
+      </div>
+      <div className="container-card_shimmer1 shine min-w-[150px] h-[280px] rounded-2xl ">
+        <div className="rest_img shine  h-[55%]"></div>
+
+        <div className="rest_info_card h-1/4">
+          <p className="name_of_hotel_shimmer wrapper_shimmer"></p>
+          <div className=" wrapper_shimmer content-line"></div>
+          <div className=" wrapper_shimmer content-line"></div>
+        </div>
+      </div>
+      <div className="container-card_shimmer1 shine min-w-[150px] h-[280px] rounded-2xl ">
+        <div className="rest_img shine  h-[55%]"></div>
+
+        <div className="rest_info_card h-1/4">
+          <p className="name_of_hotel_shimmer wrapper_shimmer"></p>
+          <div className=" wrapper_shimmer content-line"></div>
+          <div className=" wrapper_shimmer content-line"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MoreRestaurantsShimmer;
