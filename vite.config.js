266304cf1064668fa3a8eaa3d2bdc44709920d5b
Pujari Basvaraj <basvarajpujari607@gmail.com>
2024-01-27 13:43:14 +0530

    vercel issue

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react';
 import dotenv from "dotenv";
 
@@ -6,6 +6,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_CORS_PROXY),
+    },
+  }
 })
