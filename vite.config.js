e8e16f9cb4407521335411898c45ec4b174523b7
Pujari Basvaraj <basvarajpujari607@gmail.com>
2024-04-15 00:28:57 +0530

    url issue check

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
 import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react';
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-//
-dotenv.config();
+//dotenv.config();
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    // vite config
-    plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(env.VITE_CORS_PROXY),
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
+
+// export default defineConfig(({ command, mode }) => {
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '')
+//   return {
+//     // vite config
+//     plugins: [react()],
+//     define: {
+//       __APP_ENV__: JSON.stringify(env.VITE_CORS_PROXY),
+//     },
+//   }
+// })
